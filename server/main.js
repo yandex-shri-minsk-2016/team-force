f442bdaa2eba1b1f8af0984f627f36a1dcacfcb2
Sergey <sdgaykov@gmail.com>
2016-04-09 17:11:27 +0300

    hotfix for items scheme

--- server/main.js
+++ server/main.js
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import PoolsCollection from './../imports/api/pools/pools';
+import ItemsCollection from './../imports/api/items/items';
 
 Pools = PoolsCollection;
+Items = ItemsCollection;
 
 Meteor.startup(() => {
   // code to run on server at startup
