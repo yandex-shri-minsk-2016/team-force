05ff6422e4e632dfd3d228008510f576d71cf106
denisbalyko <denisbalyko@gmail.com>
2016-04-13 00:55:18 +0300

    appendPools core completed

---
+++ imports/ui/components/append_pool/append_pool.js
@@ -0,0 +1,43 @@
+import {Meteor} from 'meteor/meteor';
+import Pools from './../../../api/pools/pools';
+
+Template.appendPool.events({
+    'submit #append_pool': (event) => {
+        event.preventDefault();
+
+        const target = event.target;
+        const inputProducts = target.products.value;
+
+        if (!inputProducts) {
+            return;
+        }
+
+        let products = [];
+
+        inputProducts.split(',').forEach((product) => {
+            let newItem = {
+                shop: 'wok.by',
+                link: product,
+                createdAt: new Date()
+            };
+            products.push(newItem);
+        });
+
+        let newOrder = {
+            owner: Meteor.userId(),
+            items: products,
+            sum: 0,
+        };
+
+        poolId = FlowRouter.current().params.poolId;
+
+        CurrentPool = Pools.findOne({ _id: poolId });
+        currentOrders = CurrentPool.orders;
+        currentOrders.push(newOrder);
+
+        Pools.update(CurrentPool._id, { $set: { orders: currentOrders } });
+
+        // @TODO: notify updated Pool
+        FlowRouter.go('pool', { poolId: poolId });
+    }
+});
--- imports/ui/components/pool/pool.js
+++ imports/ui/components/pool/pool.js
@@ -1,23 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import Items from './../../../api/items/items';
 
-Template.pool.events({
-    'submit #addItem': (event) => {
-        event.preventDefault();
-
-        const inputItems = event.target.newItems.value.split(',');
-
-        if (inputItems.length === 0) return;
-
-        let items = [];
-        const poolId = ''; //TODO: Get from route
-        inputItems.forEach((item, index) => {
-            const newItem = {
-                shop: 'wok.by',
-                link: item,
-                poolId: '6hfbAucmrxbtFABAN'
-            };
-            Items.add(newItem);
-        });
+Template.pool.helpers({
+    item: () => {
+        // @TODO return instance if not defined
+        // Template.instance();
+        return false;
     }
 });
