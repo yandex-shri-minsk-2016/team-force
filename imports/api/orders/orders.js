3d71ea6ab5856b602d20890a11c4ec2fad86f3fe
denisbalyko <denisbalyko@gmail.com>
2016-04-12 22:57:48 +0300

    Order Collection completed (closed #23)

---
+++ imports/api/orders/orders.js
@@ -0,0 +1,29 @@
+import { Meteor } from 'meteor/meteor';
+import Items from './../items/items';
+
+class OrdersCollection extends Mongo.Collection {
+    constructor() {
+        super(OrdersCollection.name);
+    }
+
+    add(data, callback=null) {
+        return super.insert(data, function(error, id) {
+            if (!error) {
+                data.items.forEach((item) => {
+                    Items.add({
+                        orderId: id,
+                        link: item.link,
+                    });
+                });
+            }
+        });
+    }
+
+    findOne(filter, callback) {
+        return super.findOne(filter, callback);
+    }
+}
+
+OrdersCollection.name = 'Orders';
+
+export default new OrdersCollection();
