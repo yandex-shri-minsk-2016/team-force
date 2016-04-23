8e38556c2efecf02a97b94f1484e34ca8fa0199e
Sergey <gaykov@users.noreply.github.com>
2016-04-23 17:41:46 +0300

    dynamically add items to the poll (#50)
    * dynamically add items to the poll
    * update pr

--- imports/ui/components/pool/pool.js
+++ imports/ui/components/pool/pool.js
@@ -1,1 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import Items from './../../../api/items/items';
+
+Template.pool.helpers({
+    poolOrders: () => {
+        let orders = [];
+
+        let baseOrders = Template.instance().data.orders;
+        baseOrders.forEach((baseOrder, index) => {
+            orders.push(baseOrder);
+            baseOrder.items.forEach((baseItem, itemIndex) => {
+                orders[index].items[itemIndex] = {
+                    count: baseItem.count,
+                    item: Items.findOne({ _id: baseItem.id })
+                };
+            });
+        });
+
+        return orders;
+    }
+});
