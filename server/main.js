2084c425a46879b4bbc81af5c6add38a956518db
denisbalyko <denisbalyko@gmail.com>
2016-04-24 12:00:00 +0300

    Added companyCollection and tests (closed #30) (#51)
    Template company success/error
    
    Fix codereview
    
    + добавил красоту с тулупами на регистрации (для ввода компании)

--- server/main.js
+++ server/main.js
@@ -2,7 +2,21 @@ import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 import Pools from './../imports/api/pools/pools';
 import Items from './../imports/api/items/items';
+import Company from './../imports/api/company/company';
 
 Meteor.startup(() => {
 
+});
+
+Accounts.onCreateUser(function(options, user) {
+    if (options.profile) {
+        user.profile = options.profile;
+        try {
+            Company.add({ title: user.profile.company });
+        }catch (err) {
+            throw new Error(err);
+        }
+    }
+
+    return user;
 });
