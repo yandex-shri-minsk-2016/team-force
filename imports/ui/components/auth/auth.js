cceaf3b17fd064c4b4540b45af4f4db30da840dc
denisbalyko <denisbalyko@gmail.com>
2016-05-04 18:25:25 +0300

    issued a global to a separate file

--- imports/ui/components/auth/auth.js
+++ imports/ui/components/auth/auth.js
@@ -5,13 +5,3 @@ Template.logoutButton.events({
         });
     }
 });
-
-Template.registerHelper('usermail', (userId) => {
-    // let u = Meteor.users.findOne({ _id: userId });
-    // @TODO fix quality code
-    return 'email for: ' + userId;
-});
-
-Template.registerHelper('defEqual', (v1, v2) => {
-    return v1 == v2;
-});
