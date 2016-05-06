5038138139642c444604bddb81ab937e10719afb
denisbalyko <denisbalyko@gmail.com>
2016-05-06 23:45:04 +0300

    priceCalc template

--- imports/ui/components/common/common.js
+++ imports/ui/components/common/common.js
@@ -25,3 +25,23 @@ Template.registerHelper('isPoolOwner', (poolId) => {
 Template.registerHelper('isPoolArchived', (poolId) => {
     return utils.POOL_STATE.ARCHIVED === Pools.findOne({ _id:poolId }).state;
 });
+
+Template.registerHelper('getPriceWithFormat', (price) => {
+    return utils.getPriceWithFormat(price);
+});
+
+Template.registerHelper('getOrderPrice', (orderId) => {
+    return utils.getPriceWithFormat(Pools.getOrderPrice(orderId));
+});
+
+Template.registerHelper('getOrderIsPaidPrice', (orderId, isPaid) => {
+    return utils.getPriceWithFormat(Orders.getOrderIsPaidPrice(orderId, isPaid));
+});
+
+Template.registerHelper('getPoolPrice', (poolId) => {
+    return utils.getPriceWithFormat(Pools.getPoolPrice(poolId));
+});
+
+Template.registerHelper('getPoolIsPaidPrice', (poolId, isPaid) => {
+    return utils.getPriceWithFormat(Pools.getPoolIsPaidPrice(poolId, isPaid));
+});
