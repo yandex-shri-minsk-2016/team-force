90594f064f7b51392feafe1f7acc24a26410efeb
denisbalyko <denisbalyko@gmail.com>
2016-05-08 21:37:47 +0300

    append stylish

--- imports/ui/components/pool_pending/pool_pending.js
+++ imports/ui/components/pool_pending/pool_pending.js
@@ -12,14 +12,70 @@ Template.appendPool.helpers({
 });
 
 Template.appendPool.events({
+    'keyup .new-product-dynamic-input-group input': (event) => {
+        let isEmpty = [];
+        const joinButton = $('.join-button');
+        const hideGroup = $('.new-product-hide-input-group');
+
+        $('.new-product-dynamic-input-group input').each((input, element) => {
+            isEmpty.push(!!$(element).val());
+        });
+
+        if (isEmpty.reduce((result, current) => { return result && current; })) {
+            hideGroup.show();
+            joinButton.removeClass('disabled');
+        }else {
+            hideGroup.hide();
+            joinButton.addClass('disabled');
+        }
+    },
+
+    'click .js-new-product': (event) => {
+        let result = {
+            title: '',
+            weight:'',
+            price: '',
+            descr: ''
+        };
+
+        for (let item in result) {
+            let title = item;
+            switch (item) {
+                case 'title':
+                    title = 'Название';
+                    break;
+                case 'weight':
+                    title = 'Вес';
+                    break;
+                case 'price':
+                    title = 'Цена';
+                    break;
+                case 'descr':
+                    title = 'Описание';
+                    break;
+            }
+
+            itemFields.push({
+                title,
+                value: result[item],
+                key: `form-elem-${item}`
+            });
+        }
+
+        itemFieldsDep.changed();
+    },
+
     'paste #new-product-input': (event) => {
         const errorClass = 'has-error';
 
         const inputGroup = $('.new-product-input-group');
         const inputValue = event.originalEvent.clipboardData.getData('text');
         const joinButton = $('.join-button');
+        const hideGroup = $('.new-product-hide-input-group');
 
-        joinButton.button('loading');
+        hideGroup.hide();
+        joinButton.val('Загружаем...').parent().show();
+        joinButton.addClass('disabled');
 
         if (inputGroup.hasClass(errorClass)) {
             inputGroup.removeClass(errorClass);
@@ -57,7 +113,9 @@ Template.appendPool.events({
                 inputGroup.addClass(errorClass);
             })
             .finally(() => {
-                joinButton.button('reset');
+                hideGroup.show();
+                joinButton.val('Присоединиться');
+                joinButton.removeClass('disabled');
             });
     },
 
@@ -65,6 +123,9 @@ Template.appendPool.events({
         event.preventDefault();
 
         const target = event.target;
+        const joinButton = $('.join-button');
+        const hideGroup = $('.new-product-hide-input-group');
+        const productInput = $('#new-product-input');
 
         let newItem = {
             title: target['form-elem-title'].value,
@@ -80,6 +141,11 @@ Template.appendPool.events({
             .then(() => {
                 itemFields = [];
                 itemFieldsDep.changed();
+
+                joinButton.val('Присоединиться');
+                joinButton.addClass('disabled');
+                productInput.val('');
+                hideGroup.hide();
 
                 Router.go('pool', { poolId: poolId });
             })
